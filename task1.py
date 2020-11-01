a10b4bf0947b68e810f35b78c1d18a5c3d1dfc66
EmilioOlivastri <65777660+EmilioOlivastri@users.noreply.github.com>
2020-11-01 08:50:36 +0100

    TASK1 AND TASK2
    This two files complete both task1 and task2

--- task1.py
+++ task1.py
@@ -1,5 +1,11 @@
-import f_cipher_enc as enc
+import f_cipher as ciph
 
-cipher = enc.Feistel_Encr(32, 17, 0x80000000)
+cipher = ciph.Feistel(32, 17, 0x80000000)
 
-print(hex(cipher.encryption(0x80000000)))
+x = cipher.encryption(0x80000000)
+
+print("The messsage x = " + str(hex(x)))
+
+u = cipher.decryption(x)
+
+print("The messsage u = " + str(hex(u)))
